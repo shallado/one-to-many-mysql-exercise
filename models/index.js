4c91ed43b1f6db3d25476d3848c4f36e787d1899
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-19 21:11:51 -0700

    Configure table creation when database connection is established

--- models/index.js
+++ models/index.js
@@ -14,4 +14,30 @@ connection.connect((err) => {
   }
 
   console.log('Successfully connected to the database');
+  
+  const createCustomers = `CREATE TABLE IF NOT EXISTS customers (
+                             id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             first_name VARCHAR(155) NOT NULL,
+                             last_name VARCHAR(155) NOT NULL,
+                             email VARCHAR(155) NOT NULL
+                           )`;
+  const createOrders = `CREATE TABLE IF NOT EXISTS orders (
+                          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          order_date DATE,
+                          amount DECIMAL(8, 2),
+                          customer_id INT NOT NULL,
+                          FOREIGN KEY(customer_id) REFERENCES customers(id)
+                       )`
+
+  const tables = [createCustomers, createOrders];
+
+  for (let table of tables) {
+    connection.query(table, (err, results) => {
+      if (err) {
+        return console.log('Unable add table to the database \n', err);
+      }
+
+      console.log('Successfully added table to the database \n', results);
+    });
+  }
 });
