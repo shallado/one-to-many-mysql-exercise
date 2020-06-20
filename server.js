15b8816de9dec3985b13de631ab395ac64821b91
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-20 15:38:30 -0700

    Configure server connection

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-require('./models');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.json('Hello World'));
+
+app.listen(port, () => console.log(
+  `Successfully connected to the server on port: ${port}`)
+);
